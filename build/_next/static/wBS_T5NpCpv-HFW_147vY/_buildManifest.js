9313e4772ab4cc4469befada0007f1e4c7ee82ab
pierinanicole <113307858+pierinanicole@users.noreply.github.com>
2024-05-24 11:14:04 -0500

    Add files via upload

---
+++ build/_next/static/wBS_T5NpCpv-HFW_147vY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-9dc75c798b54cd48.js"],"/_error":["static/chunks/pages/_error-87d77e1d43fbb9db.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
